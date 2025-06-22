1631a5ea78cc849203a109f5c8e17c80b7a632b9
Karolina Anna Jesionek <karolina.anna.jesionek@gmail.com>
2025-06-22 22:04:34 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+// import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/_redirects",
-          dest: ".",
-        },
-      ],
-    }),
+    // viteStaticCopy({
+    //   targets: [
+    //     {
+    //       src: "public/_redirects",
+    //       dest: ".",
+    //     },
+    //   ],
+    // }),
   ],
 });
